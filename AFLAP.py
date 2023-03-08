507ee66c5c2d201ddf767f8a65f24fefcb116f5e
nsanchez75 <n.sanchez7580@gmail.com>
2023-03-07 16:08:57 -0800

    rewrite AFLAP.py and pedigree check program

--- AFLAP.py
+++ AFLAP.py
@@ -1,14 +1,13 @@
 import argparse
 import os
 import subprocess
+import sys
 
-import bin.check_newfiles as check_nf
-import bin.pedigree_check as pedigree_check
+import bin.ped_analysis as pa
 
-
-def main():
-    parser = argparse.ArgumentParser(prog='AFLAP', description='A script to run all stages of AFLAP.')
-    parser.add_argument('-P', '--Pedigree', required=True, help='Pedigree file (required). See AFLAP README for more information.')
+def main()->None:
+    parser = argparse.ArgumentParser(prog='AFLAP', description="A script to run all stages of AFLAP.")
+    parser.add_argument('-P', '--Pedigree', required=True, help="Pedigree file (required). See AFLAP README for more information.")
     parser.add_argument('-m', '--kmer', default=31, help='K-mer size (optional). Default [31].')
     parser.add_argument('-t', '--threads', default=4, help='Threads for JELLYFISH counting (optional). Default [4].')
     parser.add_argument('-r', '--remove', help='Individual to remove. All other options will be ignored.')
@@ -20,78 +19,59 @@ def main():
     parser.add_argument('-U', '--Max', help='Maximum number of markers to output in the genotype tables output under ./AFLAP_Results/')
     args = parser.parse_args()
 
-    # argument check
-    if args.threads is None:
-        print("Threads not specified, will proceed with default [4]")
-    if args.kmer is None:
-        print("mer size not specified, will proceed with default [31]")
 
-
-    # jellyfish check
+    # 1. check for modules
     try:
-        jellyfish_version = subprocess.check_output("jellyfish --version", shell=True)
-        jellyfish_version = jellyfish_version.decode().strip()
-        print(f"{jellyfish_version} detected\n")
+        jellyfish_version = subprocess.check_output("jellyfish --version", shell=True).decode().strip()
+        print(f"{jellyfish_version} detected.")
     except OSError:
-        print("jellyfish not detected, please modify your PATH. Terminating.")
-        exit(1)
+        print("Error: Jellyfish not detected. Please modify your PATH.")
+        sys.exit(1)
+    try:
+        abyss_version = subprocess.check_output("ABYSS --version", shell=True).decode().strip()
+        print(f"{abyss_version} detected.")
+    except OSError:
+        print("Error: ABySS not detected. Please modify your PATH.")
+        sys.exit(1)
+    # TODO: add R/4.0.1 as well?
 
 
-    # make a temporary directory
+    # 2. perform Pedigree file analysis
+    # if os.path.exists("AFLAP_tmp/Pedigree.txt"):
+    #     # TODO: check if file is same as argument input
+    #     pass
+    # else:
     os.makedirs("AFLAP_tmp", exist_ok=True)
-    # strip '#' from pedigree file and make new temporary file for it
-    with open(args.Pedigree, 'r') as infile, open("AFLAP_tmp/Pedigree.txt", 'w') as outfile:
-        for line in infile:
-            if not line.startswith('#'):
-                outfile.write(line)
-    
-    args.Pedigree = "AFLAP_tmp/Pedigree.txt"
-
-    # separate individuals in pedigree file
-    pedigree_check.separate_ped("AFLAP_tmp/Pedigree.txt")
-    # check if F1 and F2 files are formatted correctly
-    if os.path.exists("AFLAP_tmp/Pedigree_F1.txt"):
-        pedigree_check.check_format("AFLAP_tmp/Pedigree_F1.txt")
-    elif os.path.exists("AFLAP_tmp/Pedigree_F2.txt"):
-        pedigree_check.check_format("AFLAP_tmp/Pedigree_F2.txt")
-    
-    # make 01 directory
     os.makedirs("AFLAP_tmp/01", exist_ok=True)
-    # check the Pedigree file so that parents are specified
-    pedigree_check.confirm_parents()
-    # check pedigree structure (Currently only tested on F1 and F2 independently. Has not been tested on both simultaneously.)
-    pedigree_check.check_pedigree_structure()   # TODO: get rid of F2 restrictions from this function eventually
+
+    pa.pedigree_analysis(args.Pedigree)
 
 
-    if args.remove is None:     # TODO: understand what args.remove is
-        DIR = os.path.dirname(os.path.abspath(__file__))        
-        
-        os.system(f'python3 {DIR}/bin/01_JELLYFISH.py -P {args.Pedigree} -t {args.threads} -m {args.kmer}')
-        check_nf.check_01_jellyfish()
-        
-        os.system(f'python3 {DIR}/bin/02_ExtractSingleCopyMers.py -P {args.Pedigree} -m {args.kmer}')
-        check_nf.check_02_escm()
+    # 3. run programs (#TODO?: allow user to specify what programs to run)
+    if args.remove:
+        # TODO: figure out what args.remove does
+        print("Remove argument passed")
+        pass
 
-        os.system(f'python3 {DIR}/bin/03_ObtainMarkers.py -P {args.Pedigree} -m {args.kmer}')
+    DIR = os.path.dirname(os.path.abspath(__file__))
 
-        if args.LowCov:
-            os.system(f'python3 {DIR}/bin/04_Genotyping.py -P {args.Pedigree} -m {args.kmer} -L')
+    # 01_JELLYFISH.py
+    try:
+        os.system(f"python3 {DIR}/bin/01_JELLYFISH.py -t {args.threads} -m {args.kmer}")
+    except SystemExit:
+        print("Error found in 01_JELLYFISH.py.")
+        exit(1)
+    # 02_ExtractSingleCopyMers.py
+    try:
+        os.system(f"python3 {DIR}/bin/02_ExtractSingleCopyMers.py -m {args.kmer}")
+    except SystemExit:
+        print("Error found in 02_ExtractSingleCopyMers.py.")
+    # 03_ObtainMarkers.py
+    try:
+        os.system(f"python3 {DIR}/bin/03_ObtainMarkers.py")
+    except SystemExit:
+        print("Error found in 03_ObtainMarkers.py.")
 
-            print("continue to 05?")
-            exit(0)
 
-            os.system(f'{DIR}/bin/05_ObtainSegStats.sh -d {args.Sdl} -D {args.Sdu} -P {args.Pedigree} -m {args.kmer} -L')
-        else:
-            os.system(f'python3 {DIR}/bin/04_Genotyping.py -P {args.Pedigree} -m {args.kmer}')
-
-            print("continue to 05?")
-            exit(0)
-
-            os.system(f'{DIR}/bin/05_ObtainSegStats.sh -d {args.Sdl} -D {args.Sdu} -P {args.Pedigree} -m {args.kmer}')
-        if args.kinship:
-            os.system(f'{DIR}/bin/06_KinshipEstimation.sh -P {args.Pedigree}')
-        if args.Max:
-            os.system(f'{DIR}/bin/07_OutputMarkers.sh -P {args.Pedigree} -m {args.kmer} -U {args.Max}')
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
---
+++ bin/ped_analysis.py
@@ -0,0 +1,98 @@
+import os
+import sys
+
+def pedigree_analysis(pedigree: str)->None:
+    # 0. initialize variables
+    f0_count   = 0
+    f1_count   = 0
+    f2_count   = 0
+    parents    = set()
+    f1_progs   = set()
+    f2_progs   = set()
+    f1_crosses = dict()
+    f2_crosses = dict()
+
+
+    # 1. categorize pedigree into F0, F1, F2
+    with open(pedigree, 'r') as fin, open("AFLAP_tmp/Pedigree.txt", 'w') as fped, open("AFLAP_tmp/Pedigree_F0.txt", 'w') as f0, open("AFLAP_tmp/Pedigree_F1.txt", 'w') as f1, open("AFLAP_tmp/Pedigree_F2.txt", 'w') as f2:
+        # write pedigree file into Pedigree.txt
+        fped.write(f"Original File Source: {pedigree}")
+
+        for line in fin:
+            cols = line.strip().split()
+            # check line size
+            if len(cols) != 5:
+                print("Error: Line in pedigree file over 5 columns. Check pedigree file.")
+                sys.exit(1)
+
+            if cols[1] == '0':
+                if cols[0] not in parents:
+                    f0_count += 1
+                    parents.add(cols[0])
+
+                f0.write(line)
+            elif cols[1] == '1':
+                if cols[0] not in f1_progs:
+                    f1_count += 1
+                    f1_progs.add(cols[0])
+
+                # check if progeny has duplicated parents
+                if "NA" not in {cols[3], cols[4]} and cols[3] == cols[4]:
+                    print(f"Error: identical crossed parents identified for{cols[0]}.")
+                    sys.exit(1)
+                cross = str([cols[3], cols[4]].sort())
+                if cross not in f1_crosses: f1_crosses[cross] = 1
+                else: f1_crosses[cross] += 1
+
+                f1.write(line)
+            elif cols[1] == '2':
+                # TODO: work on later once we begin working on F2
+                print("Error: AFLAP does not currently work on F2 progeny.")
+                sys.exit(1)
+                f2.write(line) # for future use
+            else:
+                print("Error: Pedigree file contains individual that is not F0, F1, or F2.")
+                sys.exit(1)
+
+
+    # 2. check F1.txt and F2.txt structure
+    if os.path.exists("AFLAP_tmp/Pedigree_F1.txt"):
+        with open("AFLAP_tmp/Pedigree_F1.txt", 'r') as f:
+            for line in f:
+                line = line.strip().split()
+                if line[3] not in parents or line[4] not in parents:
+                    print(f"Error: F1 progeny {line[0]} descends from parent not found in pedigree file.")
+                    sys.exit(1)
+    if os.path.exists("AFLAP_tmp/Pedigree_F2.txt"):
+        pass    # TODO: implement when working w/ F2
+
+
+    # 3. Create F0.txt, F1.txt, F2.txt
+    with open("AFLAP_tmp/01/F0.txt", 'w') as f:
+        for p in parents: f.write(f"{p}\n")
+    with open("AFLAP_tmp/01/F1.txt", 'w') as f:
+        for f1_prog in f1_progs: f.write(f"{f1_prog}\n")
+    # TODO: implement F2.txt when working w/ F2
+
+
+    # 4. determine parent count
+    if f0_count < 2:
+        print("Error: Cannot have less than 2 parents. Check pedigree file.")
+        sys.exit(1)
+    elif f0_count == 2: print("2 parents detected. This will be easy! Identifying cross(es)...")
+    else: print(f"{f0_count} parents detected. This is not so easy! Identifying crosses...")
+
+    # 5. identify and print cross
+    with open("AFLAP_tmp/01/Crosses.txt", 'w') as f:
+        # F1 progeny
+        print("F1 crosses that have been identified:")
+        for crosses in f1_crosses:
+            c_vals = crosses.strip().split()
+            f.write(f"{f1_crosses[crosses]} 1 {c_vals[0]} {c_vals[1]}")
+        print()
+        # F2 progeny
+        # # TODO: implement stuff below when working with F2
+        # print("F1 crosses that have been identified:")
+        # for crosses in f2_crosses:
+        #     print(f"\t{crosses[0]}x{crosses[1]}")
+        #     f.write(f"{f2_crosses[crosses]} 2 {crosses[0]} {crosses[1]}")
