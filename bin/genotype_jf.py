4c0b5794ca259f17baaa9c276def636d184cad4b
nsanchez75 <n.sanchez7580@gmail.com>
2023-03-09 17:43:40 -0800

    fix {f_type}Counts to {f_type}Count

--- bin/genotype_jf.py
+++ bin/genotype_jf.py
@@ -24,14 +24,14 @@ def genotype_jfq(kmer:str, LOD:str, parent:str, lo:str, up:str, p0:str, f_type:s
 
         # perform jellyfish query
         for h in h_list:
-            if not os.path.exists(f"AFLAP_tmp/01/{f_type}Counts/{h}.jf{kmer}"):
+            if not os.path.exists(f"AFLAP_tmp/01/{f_type}Count/{h}.jf{kmer}"):
                 print(f"Error in genotype_jf.py: {h} not detected among {f_type} progeny. Rerun 01_JELLYFISH.py.")
                 sys.exit(1)
 
             if os.path.exists(f"AFLAP_tmp/04/Count/{h}_{parent}_m{kmer}_L{lo}_U{up}_{p0}.txt") and os.path.exists(f"AFLAP_tmp/04/Call/{h}_{parent}_m{kmer}_L{lo}_U{up}_{p0}.txt"):
                 print("\t")
 
-            jf_cmd = f"jellyfish query -s AFLAP_tmp/03/F0Markers/{parent}_m{kmer}_MARKERS_L{lo}_U{up}_{p0}.fa AFLAP_tmp/01/Counts/{h}.jf{kmer}"
+            jf_cmd = f"jellyfish query -s AFLAP_tmp/03/F0Markers/{parent}_m{kmer}_MARKERS_L{lo}_U{up}_{p0}.fa AFLAP_tmp/01/{f_type}Count/{h}.jf{kmer}"
             jf_out = subprocess.run(jf_cmd, shell=True, capture_output=True, text=True, executable="/bin/bash").stdout.split('\n')
             with open(f"AFLAP_tmp/04/Call/{h}_{parent}_m{kmer}_L{lo}_U{up}_{p0}.txt", 'r+') as fout:
                 # get to end of file
