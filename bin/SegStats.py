9ab987e25469bba76d4055248da9466fa14397f8
nsanchez75 <n.sanchez7580@gmail.com>
2023-03-21 18:03:31 -0700

    create python version of SegStats

---
+++ bin/SegStats.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import sys
+
+if __name__ == "__main__":
+    M61 = pd.read_table(sys.argv[1], sep=' ', header=False)
+    M62 = pd.read_table(sys.argv[2], sep=' ', header=False)
+    Mal1 = pd.read_table(sys.argv[3], sep=' ', header=False)
+
+    
