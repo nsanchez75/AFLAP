edea6185fa7d3643a00e366fa87c2ab9e4bec6b4
nsanchez75 <n.sanchez7580@gmail.com>
2023-03-21 18:04:28 -0700

    edit SegStats

--- bin/SegStats.py
+++ bin/SegStats.py
@@ -6,4 +6,4 @@ if __name__ == "__main__":
     M62 = pd.read_table(sys.argv[2], sep=' ', header=False)
     Mal1 = pd.read_table(sys.argv[3], sep=' ', header=False)
 
-    
+    M61sum = M61.sum(axis=column)
