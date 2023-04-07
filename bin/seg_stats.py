8ca4dc76cdce0fdfe405eb6556dc2e45b7b37eb1
nsanchez75 <n.sanchez7580@gmail.com>
2023-04-06 18:00:04 -0700

    convert data to (x, y)

--- bin/seg_stats.py
+++ bin/seg_stats.py
@@ -1,14 +1,21 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 import sys
 
 def normalize_data(markers:pd.DataFrame)->pd.DataFrame:
     return markers["Frequency Count"] / markers["Frequency Count"].sum()
 
-def get_seg_stats(markers_all:pd.DataFrame, markers_equal:pd.DataFrame, markers_over:pd.DataFrame, oufile_name:str)->None:
+def get_datapoints(markers:pd.DataFrame)->tuple[np.ndarray, np.ndarray]:
+    return markers["Frequency"].to_numpy(), markers["Frequency Count"].to_numpy()
+
+def get_seg_stats(markers_all:pd.DataFrame, markers_equal:pd.DataFrame, markers_over:pd.DataFrame, ak:int, oufile_name:str)->None:
     # normalize data
     markers_all     = normalize_data(markers_all)
     markers_equal   = normalize_data(markers_equal)
     markers_over    = normalize_data(markers_over)
 
-    x1 = markers_equal["Frequency"].to_numpy()
+    x1, y1 = get_datapoints(markers_equal)
+    x2, y2 = get_datapoints(markers_over)
+
+    
