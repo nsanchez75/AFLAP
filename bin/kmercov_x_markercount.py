a316f1c5e808e92646e48e84717a36549804561d
nsanchez75 <n.sanchez7580@gmail.com>
2023-04-10 19:43:57 -0700

    fix labelling

--- bin/kmercov_x_markercount.py
+++ bin/kmercov_x_markercount.py
@@ -6,8 +6,8 @@ def plot_cov_and_mcount(mc:pd.DataFrame, outfile_name:str)->None:
     y = mc["Marker Count"]
 
     plt.scatter(x, y, c='k')
-    plt.xlabel = "K-Mer Coverage"
-    plt.ylabel = "Marker Count"
+    plt.xlabel("K-Mer Coverage")
+    plt.ylabel("Marker Count")
 
     plt.savefig(outfile_name)
     plt.clf()
