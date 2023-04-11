98da52b0fc485c80746082aeeb45ef6ef48e8632
nsanchez75 <n.sanchez7580@gmail.com>
2023-04-11 11:52:21 -0700

    reformat shellscript files for easier readability

---
+++ AFLAP.sh
@@ -0,0 +1,118 @@
+#!/bin/bash
+Name=$(basename $0)
+usage="${Name}; [-h] [-P] [-t] [-m] -- A script to run all stages of AFLAP.
+
+Options
+        -h show this help message
+        -P Pedigree file, required. See AFLAP README for more information.
+        -m K-mer size. Optional. Default [31]
+        -t Threads for JELLYFISH counting. Optional. Default [4]
+	-r Individual to remove. All other options will be ignored.
+	-L LOD score - Will run LepMap3 with minimum LOD.
+	-d Lower boundary for marker cut off. Can be used to filter for segregation distortion [0.2].
+	-D Upper boundary for marker cut off. Can be used to filter for segregation distortion [0.8].
+	-k Run kinship estimation.
+	-x Run with low coverage parameters.
+	-U Maximum number of markers to output in the genotype tables output under ./AFLAP_Results/"
+
+while getopts ':kxhP:t:m:r:L:d:D:U:' option; do
+	case "$option" in
+	h)
+		echo "$usage"
+		exit
+		;;
+	P)
+		Ped=$OPTARG
+		;;
+	t)
+		thread=$OPTARG
+		;;
+	m)
+		mer=$OPTARG
+		;;
+	r)
+		rem=$OPTARG
+		;;
+	k)
+		kin=1
+		;;
+	L)
+		LOD=$OPTARG
+		;;
+	x)
+		LowCov=1
+		;;
+	d)
+		Sdl=$OPTARG
+		;;
+	D)
+		sdu=$OPTARG
+		;;
+	U)
+		Max=$OPTARG
+		;;
+	\?)
+		printf "illegal option: -%s\n\n" "$OPTARG" >&2
+		echo "$usage"
+		exit 1
+		;;
+	esac
+done
+
+if [[ -z $rem && -z $Ped ]]; then
+	echo -e "Either pedigree file (-P), or progeny individual intermediate data to remove (-r) required"
+	echo "$usage"
+	exit 1
+fi
+if [[ -z $thread ]]; then
+	echo "Threads not specified, will proceed with default [4]"
+	thread=4
+fi
+if [[ -z $Sdl ]]; then Sdl=0.2; fi
+if [[ -z $Sdu ]]; then Sdu=0.8; fi
+if [[ -z $mer ]]; then
+	echo "mer size not specified, will proceed with default [31]"
+	mer=31
+fi
+if [[ -z $rem ]]; then
+	DIR=$(dirname $0)
+	#00 Dependency check
+	echo -e "\n\e[31mBeginning Step 1/6\e[0m" &&
+		$DIR/bin/01_JELLYFISH.sh -P $Ped -t $thread -m $mer &&
+		echo -e "\n\e[31mBeginning Step 2/6\e[0m" &&
+		$DIR/bin/02_ExtractSingleCopyMers.sh -P $Ped -m $mer &&
+		echo -e "\n\e[31mBeginning Step 3/6\e[0m" &&
+		$DIR/bin/03_ObtainMarkers.sh -P $Ped -m $mer &&
+		echo -e "\n\e[31mBeginning Step 4/6\e[0m" &&
+		if [[ $LowCov == 1 ]]; then
+			$DIR/bin/04_Genotyping.sh -P $Ped -m $mer -L &&
+				echo -e "\n\e[31mBeginning Step 5/6\e[0m" &&
+				$DIR/bin/05_ObtainSegStats.sh -d $Sdl -D $Sdu -P $Ped -m $mer -L
+		else
+			$DIR/bin/04_Genotyping.sh -P $Ped -m $mer &&
+				echo -e "\n\e[31mBeginning Step 5/6\e[0m" &&
+				$DIR/bin/05_ObtainSegStats.sh -d $Sdl -D $Sdu -P $Ped -m $mer
+		fi
+	if [[ $kin == 1 ]]; then
+		echo -e "\n\e[31mRunning Kmer kinship\e[0m"
+		$DIR/bin/05b_KmerKinship.sh -P $Ped -m $mer
+	fi &&
+		if [[ -n $Max ]]; then
+			echo -e "\n\e[31mDownsampling markers\e[0m"
+			$DIR/bin/05c_MarkerReduction.sh -P $Ped -m $mer -U $Max
+		fi &&
+		echo -e "\n\e[31mBeginning Step 6/6\e[0m" &&
+		$DIR/bin/06_ExportToLepMap3.sh -P $Ped -m $mer &&
+		if [[ -z $LOD ]]; then
+			echo -e "No LOD cutoffs provided, so AFLAP will not run LepMap3. LOD cutoffs can be provided with the -L flag"
+		else
+			for g in $(echo $LOD | tr ',' '\n'); do $DIR/bin/07_LepMap3.sh -P $Ped -m $mer -T $thread -L $g; done
+		fi
+	exit
+else
+	echo -e "Removing intermediate progeny files generated for $rem"
+	find AFLAP_Intermediate/ -name "${rem}*" -exec rm {} +
+	find AFLAP_tmp/ -name "${rem}*" -exec rm {} +
+	exit
+fi
+#Add additional scripts for running LepMap3
--- bin/05_ObtainSegStats.sh
+++ bin/05_ObtainSegStats.sh
@@ -11,36 +11,45 @@ Options
 Temporary files will be output to AFLAP_tmp/05."
 #Option block
 while getopts ':LhP:m:d:D:' option; do
-        case "$option" in
-                h)  echo "$usage"
-                         exit
-                         ;;
-                P)  Ped=$OPTARG
-                         ;;
-                m)  mer=$OPTARG
-                         ;;
-		L)  CovCut=1
-		    echo "Low Coverage setting used"
-			 ;;
-		D)  SegDistU=$OPTARG
-			 ;;
-		d)  SegDistL=$OPTARG
-			 ;;
-                \?) printf "illegal option: -%s\n\n" "$OPTARG" >&2
-                    echo "$usage"
-                    exit 1
-                         ;;
-        esac
+	case "$option" in
+	h)
+		echo "$usage"
+		exit
+		;;
+	P)
+		Ped=$OPTARG
+		;;
+	m)
+		mer=$OPTARG
+		;;
+	L)
+		CovCut=1
+		echo "Low Coverage setting used"
+		;;
+	D)
+		SegDistU=$OPTARG
+		;;
+	d)
+		SegDistL=$OPTARG
+		;;
+	\?)
+		printf "illegal option: -%s\n\n" "$OPTARG" >&2
+		echo "$usage"
+		exit 1
+		;;
+	esac
 done
 
 #SetDefault CovCut
-if [[ -z $CovCut ]] ; then CovCut=2 ; echo -e "\nDefault coverage cut-off = 2." ; fi
-if [[ -e AFLAP_tmp/01/LA.txt && -e AFLAP_tmp/02/Boundaries.txt ]]
-then
-echo -e "\nIntermediate files detected"
+if [[ -z $CovCut ]]; then
+	CovCut=2
+	echo -e "\nDefault coverage cut-off = 2."
+fi
+if [[ -e AFLAP_tmp/01/LA.txt && -e AFLAP_tmp/02/Boundaries.txt ]]; then
+	echo -e "\nIntermediate files detected"
 else
-echo -e "\n Could not find output of previous scripts. Please rerun full pipeline."
-exit 1
+	echo -e "\n Could not find output of previous scripts. Please rerun full pipeline."
+	exit 1
 fi
 mkdir -p AFLAP_tmp/05/FilteredCall
 mkdir -p AFLAP_tmp/05/SegregationInformation
@@ -53,83 +62,73 @@ kk=$(printf %.0f $(echo $mer+$mer-1 | bc -l))
 DIR=$(dirname $0)
 
 #Strip '#' from pedigree file
-awk '$0 !~ /#/' $Ped > AFLAP_tmp/Pedigree.txt
+awk '$0 !~ /#/' $Ped >AFLAP_tmp/Pedigree.txt
 Ped=$DIR/AFLAP_tmp/Pedigree.txt
 
 #Loop through LA to run iteration for each parent required
-for g in `cat AFLAP_tmp/01/LA.txt`
-do
-#1 Check for Genotype table
-#Again need a low vs high coverage flag
+for g in $(cat AFLAP_tmp/01/LA.txt); do
+	#1 Check for Genotype table
+	#Again need a low vs high coverage flag
 	P0=$(cat AFLAP_tmp/03/${g}_CrossedTo.txt | tr '\n' '_' | sed 's/_$//')
 	Lo=$(awk -v var="$g" '$1 == var {print $2}' AFLAP_tmp/02/Boundaries.txt)
 	Up=$(awk -v var="$g" '$1 == var {print $3}' AFLAP_tmp/02/Boundaries.txt)
-	if [[ -e AFLAP_tmp/04/${g}_m${mer}_L${Lo}_U${Up}_$P0.Genotypes.MarkerID.tsv ]]
-	then
-#Progeny counter
+	if [[ -e AFLAP_tmp/04/${g}_m${mer}_L${Lo}_U${Up}_$P0.Genotypes.MarkerID.tsv ]]; then
+		#Progeny counter
 		ProC=$(awk 'NR == 1 {print NF-2}' AFLAP_tmp/04/${g}_m${mer}_L${Lo}_U${Up}_$P0.Genotypes.MarkerID.tsv)
 		echo "Genotype calls for $g detected. Summarizing"
-		sed 's/_/ /' AFLAP_tmp/04/${g}_m${mer}_L${Lo}_U${Up}_$P0.Genotypes.MarkerID.tsv | awk -v var="$kk" '$3 == var {for (i=4; i<=NF;i++) j+=$i; print j; j=0 }' | sort -n | uniq -c | awk -v var=$ProC '{print $2/var, $1}' > AFLAP_Results/${g}_m${mer}_L${Lo}_U${Up}_${P0}_MarkerEqual${kk}.hist
-		sed 's/_/ /' AFLAP_tmp/04/${g}_m${mer}_L${Lo}_U${Up}_$P0.Genotypes.MarkerID.tsv | awk -v var="$kk" '$3 > var {for (i=4; i<=NF;i++) j+=$i; print j; j=0 }' | sort -n | uniq -c | awk -v var=$ProC '{print $2/var, $1}' > AFLAP_Results/${g}_m${mer}_L${Lo}_U${Up}_${P0}_MarkerOver${kk}.hist
-		sed 's/_/ /' AFLAP_tmp/04/${g}_m${mer}_L${Lo}_U${Up}_$P0.Genotypes.MarkerID.tsv | awk '{for (i=4; i<=NF;i++) j+=$i; print j; j=0 }' | sort -n | uniq -c | awk -v var=$ProC '{print $2/var, $1}' > AFLAP_Results/${g}_m${mer}_L${Lo}_U${Up}_${P0}_AllMarkers.hist
-		Rscript $DIR/SegStats.R AFLAP_Results/${g}_m${mer}_L${Lo}_U${Up}_${P0}_MarkerEqual${kk}.hist AFLAP_Results/${g}_m${mer}_L${Lo}_U${Up}_${P0}_MarkerOver${kk}.hist  AFLAP_Results/${g}_m${mer}_L${Lo}_U${Up}_${P0}_AllMarkers.hist AFLAP_Results/${g}_m${mer}_L${Lo}_U${Up}_${P0}_MarkerSeg.png
-		for h in `awk -v var="$g" '$4 == var || $5 == var {print $1}' $Ped | sort -u`
-		do
-			if [[ -e AFLAP_tmp/04/Call/${h}_${g}_m${mer}_L${Lo}_U${Up}_$P0.txt ]]
-			then
-			Mcou=$(awk '{sum += $1}END{print sum}' AFLAP_tmp/04/Call/${h}_${g}_m${mer}_L${Lo}_U${Up}_$P0.txt)
-			Cov=$(awk '{print $2}' AFLAP_tmp/04/Count/${h}_${g}_m${mer}_L${Lo}_U${Up}_$P0.txt | sort -n | uniq -c | tail -n+2 | sort -nrk1,1 | head -n 1 | awk '{print $2}')
-			if [[ $Cov == 1 ]]
-				then
-				Not1=0
-				Is1=0
-				Not1=$(awk '{print $2}' AFLAP_tmp/04/Count/${h}_${g}_m${mer}_L${Lo}_U${Up}_$P0.txt | sort -n | uniq -c | awk 'NR > 5 {sum += $1}END{print sum}')
-				Is1=$(awk '{print $2}' AFLAP_tmp/04/Count/${h}_${g}_m${mer}_L${Lo}_U${Up}_$P0.txt | sort -n | uniq -c | awk 'NR == 2 {sum += $1}END{print sum}')
-				if (( $Not1 > $Is1 ))
-					then
-					Cov=$(awk '{print $2}' AFLAP_tmp/04/Count/${h}_${g}_m${mer}_L${Lo}_U${Up}_$P0.txt | sort -n | uniq -c | tail -n+6 | sort -nrk1,1 | head -n 1 | awk '{print $2}')
+		sed 's/_/ /' AFLAP_tmp/04/${g}_m${mer}_L${Lo}_U${Up}_$P0.Genotypes.MarkerID.tsv | awk -v var="$kk" '$3 == var {for (i=4; i<=NF;i++) j+=$i; print j; j=0 }' | sort -n | uniq -c | awk -v var=$ProC '{print $2/var, $1}' >AFLAP_Results/${g}_m${mer}_L${Lo}_U${Up}_${P0}_MarkerEqual${kk}.hist
+		sed 's/_/ /' AFLAP_tmp/04/${g}_m${mer}_L${Lo}_U${Up}_$P0.Genotypes.MarkerID.tsv | awk -v var="$kk" '$3 > var {for (i=4; i<=NF;i++) j+=$i; print j; j=0 }' | sort -n | uniq -c | awk -v var=$ProC '{print $2/var, $1}' >AFLAP_Results/${g}_m${mer}_L${Lo}_U${Up}_${P0}_MarkerOver${kk}.hist
+		sed 's/_/ /' AFLAP_tmp/04/${g}_m${mer}_L${Lo}_U${Up}_$P0.Genotypes.MarkerID.tsv | awk '{for (i=4; i<=NF;i++) j+=$i; print j; j=0 }' | sort -n | uniq -c | awk -v var=$ProC '{print $2/var, $1}' >AFLAP_Results/${g}_m${mer}_L${Lo}_U${Up}_${P0}_AllMarkers.hist
+		Rscript $DIR/SegStats.R AFLAP_Results/${g}_m${mer}_L${Lo}_U${Up}_${P0}_MarkerEqual${kk}.hist AFLAP_Results/${g}_m${mer}_L${Lo}_U${Up}_${P0}_MarkerOver${kk}.hist AFLAP_Results/${g}_m${mer}_L${Lo}_U${Up}_${P0}_AllMarkers.hist AFLAP_Results/${g}_m${mer}_L${Lo}_U${Up}_${P0}_MarkerSeg.png
+		for h in $(awk -v var="$g" '$4 == var || $5 == var {print $1}' $Ped | sort -u); do
+			if [[ -e AFLAP_tmp/04/Call/${h}_${g}_m${mer}_L${Lo}_U${Up}_$P0.txt ]]; then
+				Mcou=$(awk '{sum += $1}END{print sum}' AFLAP_tmp/04/Call/${h}_${g}_m${mer}_L${Lo}_U${Up}_$P0.txt)
+				Cov=$(awk '{print $2}' AFLAP_tmp/04/Count/${h}_${g}_m${mer}_L${Lo}_U${Up}_$P0.txt | sort -n | uniq -c | tail -n+2 | sort -nrk1,1 | head -n 1 | awk '{print $2}')
+				if [[ $Cov == 1 ]]; then
+					Not1=0
+					Is1=0
+					Not1=$(awk '{print $2}' AFLAP_tmp/04/Count/${h}_${g}_m${mer}_L${Lo}_U${Up}_$P0.txt | sort -n | uniq -c | awk 'NR > 5 {sum += $1}END{print sum}')
+					Is1=$(awk '{print $2}' AFLAP_tmp/04/Count/${h}_${g}_m${mer}_L${Lo}_U${Up}_$P0.txt | sort -n | uniq -c | awk 'NR == 2 {sum += $1}END{print sum}')
+					if (($Not1 > $Is1)); then
+						Cov=$(awk '{print $2}' AFLAP_tmp/04/Count/${h}_${g}_m${mer}_L${Lo}_U${Up}_$P0.txt | sort -n | uniq -c | tail -n+6 | sort -nrk1,1 | head -n 1 | awk '{print $2}')
 					else
-					Cov=$(awk '{print $2}' AFLAP_tmp/04/Count/${h}_${g}_m${mer}_L${Lo}_U${Up}_$P0.txt | sort -n | uniq -c | tail -n+2 | sort -nrk1,1 | head -n 1 | awk '{print $2}')
+						Cov=$(awk '{print $2}' AFLAP_tmp/04/Count/${h}_${g}_m${mer}_L${Lo}_U${Up}_$P0.txt | sort -n | uniq -c | tail -n+2 | sort -nrk1,1 | head -n 1 | awk '{print $2}')
+					fi
 				fi
-			fi
-			echo -e "${h}\t${Mcou}\t${Cov}"
+				echo -e "${h}\t${Mcou}\t${Cov}"
 			else
-			echo "Counts could not be found. Please rerun AFLAP"
+				echo "Counts could not be found. Please rerun AFLAP"
 			fi
-		done | awk -v OFS='\t' '{if ($2 == 0) $3 = 0; print $0}' > AFLAP_Results/${g}_m${mer}_L${Lo}_U${Up}_${P0}_MarkerCount.txt
+		done | awk -v OFS='\t' '{if ($2 == 0) $3 = 0; print $0}' >AFLAP_Results/${g}_m${mer}_L${Lo}_U${Up}_${P0}_MarkerCount.txt
 		Rscript $DIR/KmerCovXMarkerCount.R AFLAP_Results/${g}_m${mer}_L${Lo}_U${Up}_${P0}_MarkerCount.txt AFLAP_Results/${g}_m${mer}_L${Lo}_U${Up}_${P0}_KmerCovXMarkerCount.png
 		LowCov=$(awk -v CC=$CovCut '$3 < CC' AFLAP_Results/${g}_m${mer}_L${Lo}_U${Up}_${P0}_MarkerCount.txt | wc -l)
-		if (( $CovCut ==2 ))
-		then
-			if (( $LowCov >= 1 ))
-			then
-			awk -v CC=$CovCut '$3 < CC {print $1" appears to be low coverage, will be excluded"}' AFLAP_Results/${g}_m${mer}_L${Lo}_U${Up}_${P0}_MarkerCount.txt
-#Loop through the ones which are high coverage and link files into a new directory.
-#Build filtered genotype table
-#Create header.
+		if (($CovCut == 2)); then
+			if (($LowCov >= 1)); then
+				awk -v CC=$CovCut '$3 < CC {print $1" appears to be low coverage, will be excluded"}' AFLAP_Results/${g}_m${mer}_L${Lo}_U${Up}_${P0}_MarkerCount.txt
+				#Loop through the ones which are high coverage and link files into a new directory.
+				#Build filtered genotype table
+				#Create header.
 			fi
-		cd  AFLAP_tmp/05/FilteredCall
-		for v in `awk -v CC=$CovCut '$3 >= CC {print $1}' ../../../AFLAP_Results/${g}_m${mer}_L${Lo}_U${Up}_${P0}_MarkerCount.txt` 
-		do
-			ln -s ../../04/Call/${v}_${g}_m${mer}_L${Lo}_U${Up}_${P0}.txt .
-		done
-		cd ../
-		awk -v OFS='\t' '{print $2, $1}' ../04/${g}_m${mer}_L${Lo}_U${Up}_$P0.Genotypes.MarkerID.tsv | paste - FilteredCall/* | awk -v SDU=$SegDistU -v SDL=$SegDistL '{for (i=3; i<=NF;i++) j+=$i; if(j/(NF-2) >= SDL && j/(NF-2) <= SDU) print $0 ; j=0 }' - > ${g}_m${mer}_L${Lo}_U${Up}_$P0.Filtered.Genotypes.MarkerID.tsv
+			cd AFLAP_tmp/05/FilteredCall
+			for v in $(awk -v CC=$CovCut '$3 >= CC {print $1}' ../../../AFLAP_Results/${g}_m${mer}_L${Lo}_U${Up}_${P0}_MarkerCount.txt); do
+				ln -s ../../04/Call/${v}_${g}_m${mer}_L${Lo}_U${Up}_${P0}.txt .
+			done
+			cd ../
+			awk -v OFS='\t' '{print $2, $1}' ../04/${g}_m${mer}_L${Lo}_U${Up}_$P0.Genotypes.MarkerID.tsv | paste - FilteredCall/* | awk -v SDU=$SegDistU -v SDL=$SegDistL '{for (i=3; i<=NF;i++) j+=$i; if(j/(NF-2) >= SDL && j/(NF-2) <= SDU) print $0 ; j=0 }' - >${g}_m${mer}_L${Lo}_U${Up}_$P0.Filtered.Genotypes.MarkerID.tsv
 		else
-		echo -e "AFLAP ran in low coverage mode. Coverage cut-off not run. Please manually remove any isolates you wish to exclude from $Ped and rerun AFLAP.\n\nIt is possible that two peaks will be shown in AFLAP_Results/${g}_m${mer}_L${Lo}_U${Up}_${P0}_MarkerSeg.png\nIf that is the case please rerun AFLAP.sh providing -d and -D for lower and upper limits for marker filtering."
-		cd  AFLAP_tmp/05/FilteredCall
-		for v in `awk '{print $1}' ../../../AFLAP_Results/${g}_m${mer}_L${Lo}_U${Up}_${P0}_MarkerCount.txt` 
-		do
-			ln -s ../../04/Call/${v}_${g}_m${mer}_L${Lo}_U${Up}_${P0}.txt .
-		done
-		cd ../
-		awk -v OFS='\t' '{print $2, $1}' ../04/${g}_m${mer}_L${Lo}_U${Up}_$P0.Genotypes.MarkerID.tsv | paste - FilteredCall/* | awk -v SDU=$SegDistU -v SDL=$SegDistL '{for (i=3; i<=NF;i++) j+=$i; if(j/(NF-2) >= SDL && j/(NF-2) <= SDU) print $0 ; j=0 }' - > ${g}_m${mer}_L${Lo}_U${Up}_$P0.Filtered.Genotypes.MarkerID.tsv
+			echo -e "AFLAP ran in low coverage mode. Coverage cut-off not run. Please manually remove any isolates you wish to exclude from $Ped and rerun AFLAP.\n\nIt is possible that two peaks will be shown in AFLAP_Results/${g}_m${mer}_L${Lo}_U${Up}_${P0}_MarkerSeg.png\nIf that is the case please rerun AFLAP.sh providing -d and -D for lower and upper limits for marker filtering."
+			cd AFLAP_tmp/05/FilteredCall
+			for v in $(awk '{print $1}' ../../../AFLAP_Results/${g}_m${mer}_L${Lo}_U${Up}_${P0}_MarkerCount.txt); do
+				ln -s ../../04/Call/${v}_${g}_m${mer}_L${Lo}_U${Up}_${P0}.txt .
+			done
+			cd ../
+			awk -v OFS='\t' '{print $2, $1}' ../04/${g}_m${mer}_L${Lo}_U${Up}_$P0.Genotypes.MarkerID.tsv | paste - FilteredCall/* | awk -v SDU=$SegDistU -v SDL=$SegDistL '{for (i=3; i<=NF;i++) j+=$i; if(j/(NF-2) >= SDL && j/(NF-2) <= SDU) print $0 ; j=0 }' - >${g}_m${mer}_L${Lo}_U${Up}_$P0.Filtered.Genotypes.MarkerID.tsv
 		fi
-	ls FilteredCall/* | sed "s/_${g}.*//" | sed 's/.*\///' > ${g}_m${mer}_L${Lo}_U${Up}_$P0.ProgHeader.txt
-	cat <(cat ${g}_m${mer}_L${Lo}_U${Up}_$P0.ProgHeader.txt | tr '\n' '\t' | sed 's/\t$//' | awk -v OFS='\t' '{print "MarkerID", "MakerSeq", $0}') ${g}_m${mer}_L${Lo}_U${Up}_$P0.Filtered.Genotypes.MarkerID.tsv > ../../AFLAP_Results/${g}_m${mer}_L${Lo}_U${Up}_$P0.GT.tsv
-#Removing means isolates can be excluded by editing the Pedigree file
-	rm FilteredCall/*
-	cd ../../
+		ls FilteredCall/* | sed "s/_${g}.*//" | sed 's/.*\///' >${g}_m${mer}_L${Lo}_U${Up}_$P0.ProgHeader.txt
+		cat <(cat ${g}_m${mer}_L${Lo}_U${Up}_$P0.ProgHeader.txt | tr '\n' '\t' | sed 's/\t$//' | awk -v OFS='\t' '{print "MarkerID", "MakerSeq", $0}') ${g}_m${mer}_L${Lo}_U${Up}_$P0.Filtered.Genotypes.MarkerID.tsv >../../AFLAP_Results/${g}_m${mer}_L${Lo}_U${Up}_$P0.GT.tsv
+		#Removing means isolates can be excluded by editing the Pedigree file
+		rm FilteredCall/*
+		cd ../../
 	else
 		echo "Draft genotype table for $g not found. Please rerun the pipeline"
 		exit 1
