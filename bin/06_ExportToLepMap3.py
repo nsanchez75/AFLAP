4fee8f535b816aa29b230e984e0e78c55a591f1c
Nikko Sanchez <n.sanchez7580@gmail.com>
2023-04-24 16:18:24 -0700

    use insert

--- bin/06_ExportToLepMap3.py
+++ bin/06_ExportToLepMap3.py
@@ -42,12 +42,14 @@ if __name__ == "__main__":
                     sex_dict['female']  = G
                 break
 
-        data =  pd.DataFrame([["CHR", "POS", f"{sex_dict['male']}x{sex_dict['female']}", f"{sex_dict['male']}x{sex_dict['female']}"],
-                              ["CHR", "POS", sex_dict['male']                          , sex_dict['female']                        ],
-                              ["CHR", "POS", '0'                                       , '0'                                       ],
-                              ["CHR", "POS", '0'                                       , '0'                                       ],
-                              ["CHR", "POS", '1'                                       , '2'                                       ],
-                              ["CHR", "POS", '0'                                       , '0'                                       ]])
+        data =  [["CHR", "POS", f"{sex_dict['male']}x{sex_dict['female']}", f"{sex_dict['male']}x{sex_dict['female']}"],
+                 ["CHR", "POS", sex_dict['male']                          , sex_dict['female']                        ],
+                 ["CHR", "POS", '0'                                       , '0'                                       ],
+                 ["CHR", "POS", '0'                                       , '0'                                       ],
+                 ["CHR", "POS", '1'                                       , '2'                                       ],
+                 ["CHR", "POS", '0'                                       , '0'                                       ]]
+
+        df = pd.DataFrame(data)
 
         # put all F1 progeny of parent G into data header
         with open("AFLAP_tmp/Pedigree_F1.txt", 'r') as fprog1:
@@ -56,16 +58,12 @@ if __name__ == "__main__":
                 p1 = p1.strip().split()
 
                 if G in (p1[3], p1[4]) and p1[0] not in p1_set:
-                    prog_data = np.chararray([[f"{sex_dict['male']}x{sex_dict['female']}"],
-                                              [p1[0]                                     ],
-                                              [sex_dict['male']                          ],
-                                              [sex_dict['female']                        ],
-                                              ['0'                                       ],
-                                              ['0'                                       ]])
-                    np.hstack(data, prog_data)
-                
+                    added_data = [f"{sex_dict['male']}x{sex_dict['female']}", p1[0],
+                                    sex_dict['male'], sex_dict['female'], '0', '0']
+                    df.insert(len(df.columns), len(df.columns), added_data)
+
                 p1_set.add(p1[0])
-                    
+
 
         print(data)
 
