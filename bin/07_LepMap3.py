5fb7fc356ef5e600b3480f4360d93e24e02cc2a6
Nikko Sanchez <n.sanchez7580@gmail.com>
2023-04-27 10:54:29 -0700

    implement LepMap3 sepchromosomes

--- bin/07_LepMap3.py
+++ bin/07_LepMap3.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+import subprocess
 
 import get_LA_info as gli
 
@@ -16,6 +17,27 @@ if __name__ == "__main__":
     
     # make directory
     os.makedirs(f"AFLAP_Results/LOD{args.LOD}", exist_ok=True)
-    
+
     # run LepMap3 on parents
-    gli.get_LA_info("AFLAP_tmp/")
+    list_of_Gs = gli.get_LA_info()
+    for G_info in list_of_Gs:
+        G, LO, UP, P0 = G_info
+
+        # check if tsv file for G exists
+        if not os.path.exists(f"AFLAP_Results/{G}_m{args.kmer}_L{LO}_U{UP}_{P0}.ForLepMap3.tsv"):
+            raise FileNotFoundError(f"Error: LepMap-ready genotype table fo {G} not found.")
+
+        # begin LepMap3 analysis
+        print(f"Initiating LepMap3 analysis on {G}...")
+        ## skip if G had already been analyzed
+        if os.path.exists(f"AFLAP_Results/LOD{args.LOD}/{G}_m{args.kmer}_L{LO}_U{UP}_{P0}.LOD{args.LOD}.txt"):
+            print(f"\tPrevious results for {G} detected. Skipping.")
+        else:
+            # run LepMap3 - SeparateChromosomes2
+            sc2_stdout = open(f"AFLAP_Results/LOD{args.LOD}/{G}_m{args.kmer}_L{LO}_U{UP}_{P0}.LOD{args.LOD}.txt", 'w')
+            sc2_stderr = open(f"AFLAP_Results/LOD{args.LOD}/{G}_m{args.kmer}_L{LO}_U{UP}_{P0}.LOD{args.LOD}.stderr", 'w')
+            subprocess.run(f"java -cp $CONDA_PREFIX/bin/lepmap3/ SeparateChromosomes2 data=AFLAP_Results/{G}_m{args.kmer}_L{LO}_U{UP}_${P0}.ForLepMap3.tsv lodLimit=$LOD numThreads=$Threads",
+                           stdout=sc2_stdout, stderr=sc2_stderr)
+            sc2_stdout.close(), sc2_stderr.close()
+
+        print("continue coding 07?")
