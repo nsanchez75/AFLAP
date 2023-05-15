58cc504a69fbdaa47178fe78dfe1b66348f900ee
Nikko Sanchez <n.sanchez7580@gmail.com>
2023-05-15 16:09:47 -0700

    update yon function

--- bin/helper_funcs/yon.py
+++ bin/helper_funcs/yon.py
@@ -1,7 +1,6 @@
 def y_or_n(prompt:str)->bool:
     while(True):
-        usr_input = (input(prompt)).lower()
+        usr_input = (input(f"{prompt} ")).lower()
 
         if   usr_input in ("y", "ye", "yes"): return True
         elif usr_input in ("n", "no"):        return False
-        else: print("Invalid input. (y/n)")
