76d04b952926969052c0418ed24b9a53ef84e4d8
Nikko Sanchez <n.sanchez7580@gmail.com>
2023-05-25 11:46:52 -0700

    edit comments

--- bin/jellyfish_count.py
+++ bin/jellyfish_count.py
@@ -11,6 +11,7 @@ def jellyfish_count(kmer:str, threads:str, f_type:str)->None:
     ped_df = pd.read_csv(f"AFLAP_tmp/Pedigree_{f_type}.txt", sep='\t',
                          names=['Individual', 'Generation', 'File', 'LB', 'UB'])
 
+    # run jellyfish count on each individual
     for ind in ped_df['Individual'].unique():
         # check if individual's count exists
         if os.path.exists(f"AFLAP_tmp/01/{f_type}Count/{ind}.jf{kmer}"):
@@ -39,39 +40,5 @@ def jellyfish_count(kmer:str, threads:str, f_type:str)->None:
         if not os.path.exists(f"AFLAP_tmp/01/{f_type}Count/{ind}.jf{kmer}"):
             raise FileNotFoundError(f"Jellyfish for {ind} did not complete.")
         print(f"\tHash for {ind} performed.")
-
-    # with open(f"AFLAP_tmp/Pedigree_{f_type}.txt", 'r') as f:
-    #     for ind in f:
-    #         ind = ind.strip().split()[0]
-
-    #         # check if individual's count exists
-    #         if os.path.exists(f"AFLAP_tmp/01/{f_type}Count/{ind}.jf{kmer}"):
-    #             if not os.path.getsize(f"AFLAP_tmp/01/{f_type}Count/{ind}.jf{kmer}"):
-    #                 raise ValueError(f"AFLAP_tmp/01/{f_type}Count/{ind}.jf{kmer} is empty. " +
-    #                                  f"Delete it and restart.")
-
-    #             print(f"\tHash detected for {ind}. Skipping.")
-    #         else:
-    #             print(f"\tRunning jellyfish count for {ind}...")
-
-    #             reads = []
-    #             with open(f"AFLAP_tmp/Pedigree_{f_type}.txt", 'r') as fin:
-    #                 for line in fin:
-    #                     line = line.strip().split()
-    #                     if line[0] == ind:
-    #                         if not os.path.exists(line[2]):
-    #                             raise FileNotFoundError(f"{line[2]} for {ind} not found.")
-    #                         reads.append(line[2])
-
-    #             # run jellyfish command
-    #             jfin = ' '.join(reads)
-    #             cmd = f"jellyfish count -m {kmer} -C -s 1G -t {threads} -o AFLAP_tmp/01/{f_type}Count/{ind}.jf{kmer} <(zcat {jfin})"
-    #             subprocess.run(cmd, shell=True, executable="/bin/bash")
-
-    #             # check if jellyfish worked
-    #             if not os.path.exists(f"AFLAP_tmp/01/{f_type}Count/{ind}.jf{kmer}"):
-    #                 raise FileNotFoundError(f"Jellyfish for {ind} did not complete.")
-    #             else:
-    #                 print(f"\tHash for {ind} performed.")
 
     print(f"Jellyfish {f_type} count complete.")
