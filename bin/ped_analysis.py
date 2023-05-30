6a712e0ea7b1461936645d947f75f19dc99c0a99
Nikko Sanchez <n.sanchez7580@gmail.com>
2023-05-30 12:36:56 -0700

    print parents set

--- bin/ped_analysis.py
+++ bin/ped_analysis.py
@@ -3,6 +3,7 @@ import shutil
 
 def check_prog(prog_info:pd.DataFrame, parents:set, cross_dict:dict)->dict:
     # check if parents unidentified
+    print(parents)
     print(prog_info.loc[(~prog_info["MP"].isin(parents)) | (~prog_info["FP"].isin(parents))])
     if prog_info.loc[(~prog_info["MP"].isin(parents)) | (~prog_info["FP"].isin(parents))].empty:
         raise ValueError(f"A progeny comes from parent not found in the pedigree file.")
