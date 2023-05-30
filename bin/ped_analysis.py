661588347c2c64642c8ee096b25fdcbde99b9de5
Nikko Sanchez <n.sanchez7580@gmail.com>
2023-05-30 11:33:07 -0700

    create csvs

--- bin/ped_analysis.py
+++ bin/ped_analysis.py
@@ -44,9 +44,9 @@ def pedigree_analysis(pedigree: str)->None:
         f2_progs = ped_df.loc[ped_df["Generation"].astype(int) == 2]
 
         # create categorized pedigree files
-        parents.to_csv(sep='\t', header=None, index=False)
-        f1_progs.to_csv(sep='\t', header=None, index=False)
-        f2_progs.to_csv(sep='\t', header=None, index=False)
+        parents.to_csv("AFLAP_tmp/Pedigree_F0.txt", sep='\t', header=None, index=False)
+        f1_progs.to_csv("AFLAP_tmp/Pedigree_F1.txt", sep='\t', header=None, index=False)
+        f2_progs.to_csv("AFLAP_tmp/Pedigree_F2.txt", sep='\t', header=None, index=False)
 
         # initialize cross dictionaries
         f1_crosses = dict()
