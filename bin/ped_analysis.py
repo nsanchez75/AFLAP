e5ce56dc68ec600bb687acd9427868047f0a0959
Nikko Sanchez <n.sanchez7580@gmail.com>
2023-05-30 11:40:40 -0700

    add parentheses

--- bin/ped_analysis.py
+++ bin/ped_analysis.py
@@ -44,8 +44,8 @@ def pedigree_analysis(pedigree: str)->None:
         f2_progs = ped_df.loc[ped_df["Generation"].astype(int) == 2]
 
         # create LA.txt from parents
-        la_df = parents.loc[parents["MP"] != "NA" & parents["FP"] != "NA"]
-        nola_df = parents.loc[~(parents["MP"] != "NA" & parents["FP"] != "NA")]
+        la_df = parents.loc[(parents["MP"] != "NA") & (parents["FP"] != "NA")]
+        nola_df = parents.loc[~((parents["MP"] != "NA") & (parents["FP"] != "NA"))]
         print(la_df)
         print(nola_df)
 
