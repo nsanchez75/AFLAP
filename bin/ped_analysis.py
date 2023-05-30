d9897478877a029bdf0e264c9d5c846a248d8136
Nikko Sanchez <n.sanchez7580@gmail.com>
2023-05-30 13:31:17 -0700

    try reset_index()

--- bin/ped_analysis.py
+++ bin/ped_analysis.py
@@ -10,8 +10,10 @@ def check_prog(prog_info:pd.DataFrame, ftype:int, parents:list, cross_dict:dict)
     if (prog_info["MP"].astype(str) == prog_info["FP"].astype(str)).any():
         raise ValueError(f"Identical crossed parents for an F{ftype} progeny identified.")
     
-    count_crosses = prog_info.groupby(["MP", "FP"]).size().sum()
+    # find number of crosses with the 
+    count_crosses = prog_info.groupby(["MP", "FP"]).size().reset_index()
     print(count_crosses)
+    
 
     return cross_dict
 
