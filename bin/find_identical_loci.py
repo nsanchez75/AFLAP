e04146a83cc5401984625f19f5c43820b0879efb
Nikko Sanchez <n.sanchez7580@gmail.com>
2023-07-02 18:34:53 -0700

    create find_identical_loci tester

--- bin/find_identical_loci.py
+++ bin/find_identical_loci.py
@@ -1,9 +1,6 @@
 import pandas as pd
 
-def find_identifier(parents_set:set, useq_df:pd.DataFrame)->str:
-    identifier = useq_df["Identifier"][useq_df["Parent"].isin(parents_set)].tolist()
-    if len(identifier) != 1: raise ValueError("Invalid number of parents of the same sex for one individual.")
-    return identifier[0]
+from get_LA_info import get_LA_info
 
 def find_identical_loci(seq_groups:pd.DataFrame)->None:
     print("Finding identical sequence loci...")
@@ -14,12 +11,44 @@ def find_identical_loci(seq_groups:pd.DataFrame)->None:
     FP = set(crosses_df["FP"].unique())
 
     # create a dataframe of same loci sequences
-    seqs_df = pd.DataFrame(columns=["Sequence", "Male Identifier", "Female Identifier"])
-    for useq in seq_groups["Sequence"].unique():
-        useq_df = seq_groups[seq_groups["Sequence"] == useq]
-        male_identifier = find_identifier(MP, useq_df)
-        female_identifier = find_identifier(FP, useq_df)
-        seqs_df.loc[len(seqs_df.index)] = [useq, male_identifier, female_identifier]
+    seqs_df = pd.DataFrame(columns=["Sequence"])
+    for useq in seq_groups["Locus Sequence"].unique():
+        useq_df = seq_groups[seq_groups["Locus Sequence"] == useq]
+        parents = useq_df["Parent"].unique().tolist()
+        # detect if both parents identified to locus sequence
+        mpcheck = False
+        fpcheck = False
+        for p in parents:
+            if p in MP: mpcheck = True
+            if p in FP: fpcheck = True
+        # if identified add sequences associated to locus sequence to tsv file dataframe
+        if mpcheck and fpcheck:
+            for seq in useq_df["Sequence"].unique():
+                if seq not in set(seqs_df["Sequence"].unique()):
+                    seqs_df.loc[len(seqs_df.index)] = [seq]
 
     # produce a tsv file of same-loci sequences
     seqs_df.to_csv("AFLAP_tmp/03/SameLociSeqs.tsv", sep='\t', index=False)
+
+if __name__ == "__main__":
+    seq_groups = pd.DataFrame(columns=["Sequence", "Locus Sequence", "Parent"])
+    kmer = 31
+    ak = 2 * kmer - 1
+
+    for G_info in get_LA_info():
+        G, LO, UP, P0 = G_info
+        with open(f"AFLAP_tmp/03/{G}_m{kmer}_L{LO}_U{UP}_abyss.fa", 'r') as fab:
+            while True:
+                id = fab.readline().strip()
+                seq = fab.readline().strip()
+                if not id or not seq: break
+
+                id = id.replace('>', '').split()
+                if int(id[1]) >= ak:
+                    subseq = seq[9:(9 + kmer)]
+                    rc_subseq = subseq[::-1].translate(subseq.maketrans("ATCG", "TAGC"))
+                    if subseq > rc_subseq: subseq = rc_subseq
+                
+                    seq_groups.loc[len(seq_groups.index)] = [subseq, seq[0:(kmer - 1)] + seq[(len(seq) - kmer + 1):], G]
+    
+    find_identical_loci(seq_groups)
