97939d694131d11e690b00c08a9d0b49153311d0
Nikko Sanchez <n.sanchez7580@gmail.com>
2023-07-05 18:00:00 -0700

    reformat 04

--- bin/04_Genotyping.py
+++ bin/04_Genotyping.py
@@ -18,52 +18,55 @@ def genotype_jfq(kmer:str, LowCov:str, parent:str, lo:str, up:str, p0:str, f_typ
     h_list = []
 
     # add progeny of parent to list
-    if os.path.exists(f"AFLAP_tmp/Pedigree_{f_type}.txt"):
-        with open(f"AFLAP_tmp/Pedigree_{f_type}.txt") as f:
-            prog_set = set()
-            for prog in f:
-                prog = prog.strip().split()
+    ped_file = f"AFLAP_tmp/Pedigree_{f_type}.txt"
+    if not os.path.exists(ped_file):
+        exit(f"An error occurred: {ped_file} not found. Rerun AFLAP.py")
 
-                if parent in {prog[3], prog[4]} and prog[0] not in prog_set:
-                    h_list.append(prog[0])
-                    prog_set.add(prog[0])
+    with open(f"AFLAP_tmp/Pedigree_{f_type}.txt") as f:
+        prog_set = set()
+        for prog in f:
+            prog = prog.strip().split()
 
-        # check if any progeny found
-        if not len(h_list):
-            print(f"\t\tNo progeny of {parent} found among given {f_type}.")
+            if parent in {prog[3], prog[4]} and prog[0] not in prog_set:
+                h_list.append(prog[0])
+                prog_set.add(prog[0])
 
-        # perform jellyfish query if necessary
-        for h in h_list:
-            print(f"\t\tCreating Count and Call for {h}...")
-            if not os.path.exists(f"AFLAP_tmp/01/{f_type}Count/{h}.jf{kmer}"):
-                exit(f"Error in genotype_jf.py: {h} not detected among {f_type} progeny. Rerun 01_JELLYFISH.py.")
+    # check if any progeny found
+    if not len(h_list):
+        print(f"\t\tNo progeny of {parent} found among given {f_type}.")
 
-            # create counts of progeny
-            count_file = f"AFLAP_tmp/04/Count/{h}_{parent}_m{kmer}_L{lo}_U{up}_{p0}.txt"
-            if os.path.exists(count_file) and os.path.getsize(count_file):
-                print(f"\t\t\tCount for {h} detected. Skipping")
-            else:
-                jf_cmd = f"jellyfish query -s AFLAP_tmp/03/F0Markers/{parent}_m{kmer}_MARKERS_L{lo}_U{up}_{p0}.fa AFLAP_tmp/01/{f_type}Count/{h}.jf{kmer}"
-                jf_out = subprocess.run(jf_cmd, shell=True, capture_output=True, text=True, executable="/bin/bash").stdout.split('\n')
-                with open(count_file, 'w') as f:
-                    for line in jf_out:
-                        # disregard empty lines FIXME: determine why this happens
-                        if not len(line): continue
-                        f.write(f"{line}\n")
-                print(f"\t\t\tCount for {h} created.")
+    # perform jellyfish query if necessary
+    for h in h_list:
+        print(f"\t\tCreating Count and Call for {h}...")
+        if not os.path.exists(f"AFLAP_tmp/01/{f_type}Count/{h}.jf{kmer}"):
+            exit(f"An error occurred: {h} not detected among {f_type} progeny. Rerun 01_JELLYFISH.py.")
 
-            # create calls of progeny
-            call_file = f"AFLAP_tmp/04/Call/{h}_{parent}_m{kmer}_L{lo}_U{up}_{p0}.txt"
-            if os.path.exists(call_file) and os.path.getsize(call_file):
-                print(f"\t\t\tCall for {h} detected. Skipping.")
-            else:
-                with open(count_file, 'r') as fcount, open(call_file, 'w') as fcall:
-                    for line in fcount:
-                        line = line.strip().split()
+        # create counts of progeny
+        count_file = f"AFLAP_tmp/04/Count/{h}_{parent}_m{kmer}_L{lo}_U{up}_{p0}.txt"
+        if os.path.exists(count_file) and os.path.getsize(count_file):
+            print(f"\t\t\tCount for {h} detected. Skipping")
+        else:
+            jf_cmd = f"jellyfish query -s AFLAP_tmp/03/F0Markers/{parent}_m{kmer}_MARKERS_L{lo}_U{up}_{p0}.fa AFLAP_tmp/01/{f_type}Count/{h}.jf{kmer}"
+            jf_out = subprocess.run(jf_cmd, shell=True, capture_output=True, text=True, executable="/bin/bash").stdout.split('\n')
+            with open(count_file, 'w') as f:
+                for line in jf_out:
+                    # disregard empty lines FIXME: determine why this happens
+                    if not len(line): continue
+                    f.write(f"{line}\n")
+            print(f"\t\t\tCount for {h} created.")
 
-                        if int(line[1]) >= int(LowCov): fcall.write(f"1\n")
-                        else: fcall.write(f"0\n")
-                print(f"\t\t\tCall for {h} created.")
+        # create calls of progeny
+        call_file = f"AFLAP_tmp/04/Call/{h}_{parent}_m{kmer}_L{lo}_U{up}_{p0}.txt"
+        if os.path.exists(call_file) and os.path.getsize(call_file):
+            print(f"\t\t\tCall for {h} detected. Skipping.")
+        else:
+            with open(count_file, 'r') as fcount, open(call_file, 'w') as fcall:
+                for line in fcount:
+                    line = line.strip().split()
+
+                    if int(line[1]) >= int(LowCov): fcall.write(f"1\n")
+                    else: fcall.write(f"0\n")
+            print(f"\t\t\tCall for {h} created.")
 
     return h_list
 
