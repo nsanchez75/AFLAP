1d41ab61df6d3eb32408e3b7cb2bbb987122d775
Nikko Sanchez <n.sanchez7580@gmail.com>
2023-07-05 17:34:07 -0700

    reformat 02

--- bin/02_ExtractSingleCopyMers.py
+++ bin/02_ExtractSingleCopyMers.py
@@ -12,26 +12,18 @@ from histoplot import histoplot
 #################################################
 
 def create_histogram(G:int, kmer:int)->None:
-    if os.path.exists(f"AFLAP_tmp/02/F0Histo/{G}.{kmer}.histo") and os.path.getsize(f"AFLAP_tmp/02/F0Histo/{G}.{kmer}.histo"):
+    histo_file = f"AFLAP_tmp/02/F0Histo/{G}.{kmer}.histo"
+    if os.path.exists(histo_file) and os.path.getsize(histo_file):
         print(f"\tHistogram for {G} detected. Skipping.")
-    else:
-        if os.path.exists(f"AFLAP_tmp/02/F0Histo/{G}.{kmer}.histo") and not os.path.getsize(f"AFLAP_tmp/02/F0Histo/{G}.{kmer}.histo"):
-            print(f"\tEmpty histogram for {G} found. Deleting...")
-            subprocess.run(f"rm AFLAP_tmp/02/F0Histo/{G}.{kmer}.histo", shell=True)
-            print(f"\tCreating new histogram for {G}...")
-        else:
-            print(f"\tCreating histogram for {G}...")
+        return
+    print(f"\tCreating histogram for {G}...")
 
-        subprocess.run(f"jellyfish histo AFLAP_tmp/01/F0Count/{G}.jf{kmer} > AFLAP_tmp/02/F0Histo/{G}.{kmer}.histo",
-                       shell=True, executable="/bin/bash")
-
-        # check if jellyfish histo worked properly
-        if not os.path.exists(f"AFLAP_tmp/02/F0Histo/{G}.{kmer}.histo"):
-            exit(f"An error occurred: Jellyfish did not produce AFLAP_tmp/02/F0Histo/{G}.{kmer}.histo.")
-        if not os.path.getsize(f"AFLAP_tmp/02/F0Histo/{G}.{kmer}.histo"):
-            exit(f"An error occurred: AFLAP_tmp/02/F0Histo/{G}.{kmer}.histo is empty.")
-
-        print(f"\tHistogram for {G} generated.")
+    subprocess.run(f"jellyfish histo AFLAP_tmp/01/F0Count/{G}.jf{kmer} > AFLAP_tmp/02/F0Histo/{G}.{kmer}.histo",
+                    shell=True, executable="/bin/bash")
+    # check if jellyfish histo worked properly
+    if not os.path.exists(histo_file) or not os.path.getsize(histo_file):
+        exit(f"An error occurred: Jellyfish did not create AFLAP_tmp/02/F0Histo/{G}.{kmer}.histo properly.")
+    print(f"\tHistogram for {G} generated.")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog='ExtractingSingleCopyMers', description="A script to obtain single copy k-mers from parental JELLYFISH hashes.")
@@ -59,30 +51,32 @@ if __name__ == "__main__":
 
         # extract k-mers
         print(f"\tExtracting {args.kmer}-mers from {G}:")
-        if os.path.exists(f"AFLAP_tmp/02/{G}_m{args.kmer}_L{LO}_U{UP}.fa"):
+        fa_file = f"AFLAP_tmp/02/{G}_m{args.kmer}_L{LO}_U{UP}.fa"
+        if os.path.exists(fa_file):
             print(f"\t\t{args.kmer}-mers for {G} detected. Skipping.")
             histo_same = True
         else:
             print(f"\t\tRunning jellyfish dump for {G}...")
-            cmd = f"jellyfish dump -U {UP} -L {LO} -o AFLAP_tmp/02/{G}_m{args.kmer}_L{LO}_U{UP}.fa AFLAP_tmp/01/F0Count/{G}.jf{args.kmer}"
+            cmd = f"jellyfish dump -U {UP} -L {LO} -o {fa_file} AFLAP_tmp/01/F0Count/{G}.jf{args.kmer}"
             subprocess.run(cmd, shell=True, executable="/bin/bash")
             histo_same = False
 
         # counting k-mers
         print(f"\tCounting number of {args.kmer}-mers for {G}...")
-        print(f"\t\t{int(os.path.getsize(f'AFLAP_tmp/02/{G}_m{args.kmer}_L{LO}_U{UP}.fa') / 2)} {args.kmer}-mers counted for {G}")
+        print(f"\t\t{int(os.path.getsize(fa_file) / 2)} {args.kmer}-mers counted for {G}")
 
         # create histo.png
-        if os.path.exists(f"AFLAP_Results/Plots/{G}_m{args.kmer}_L{LO}_U{UP}_histo.png") and histo_same:
+        png_file = f"AFLAP_Results/Plots/{G}_m{args.kmer}_L{LO}_U{UP}_histo.png"
+        if os.path.exists(png_file) and histo_same:
             print(f"\tHistogram for {G} detected. Skipping.")
         else:
             if not histo_same:
                 print(f"\tMaking new histogram for {G}...")
             else:
                 print(f"\t Making histogram for {G}...")
-            histoplot(f"AFLAP_tmp/02/F0Histo/{G}.{args.kmer}.histo", LO, UP, f"AFLAP_Results/Plots/{G}_m{args.kmer}_L{LO}_U{UP}_histo.png")
+            histoplot(f"AFLAP_tmp/02/F0Histo/{G}.{args.kmer}.histo", LO, UP, png_file)
 
             # check if histogram had been built
-            if not os.path.exists(f"AFLAP_Results/Plots/{G}_m{args.kmer}_L{LO}_U{UP}_histo.png"):
-                exit(f"An error occurred: AFLAP_Results/Plots/{G}_m{args.kmer}_L{LO}_U{UP}_histo.png not found.")
+            if not os.path.exists(png_file) or not os.path.getsize(png_file):
+                exit(f"An error occurred: {png_file} not found.")
             print(f"\tHistogram for {G} constructed.")
