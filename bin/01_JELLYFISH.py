615cf7fd7ee04712073451bcba87bad76002ab43
Nikko Sanchez <n.sanchez7580@gmail.com>
2023-07-10 17:49:19 -0700

    transfer jellyfish_count to 01

--- bin/01_JELLYFISH.py
+++ bin/01_JELLYFISH.py
@@ -1,7 +1,7 @@
 import argparse
 import os
-
-from jellyfish_count import jellyfish_count
+import pandas as pd
+import subprocess
 
 ###########################################################
 #	A Python script to be ran as part of the AFLAP pipeline.
@@ -9,6 +9,39 @@ from jellyfish_count import jellyfish_count
 #	The output will be stored in AFLAP_tmp.
 #	The script will detect previous results and used them when able.
 ###########################################################
+
+def jellyfish_count(kmer:str, threads:str, f_type:str)->None:
+    if not os.path.getsize(f"AFLAP_tmp/Pedigree_{f_type}.txt"):
+        print(f"No {f_type} individuals detected.")
+        return
+    print(f"Performing jellyfish {f_type} count:")
+
+    # run jellyfish count on each individual
+    ped_df = pd.read_csv(f"AFLAP_tmp/Pedigree_{f_type}.txt", sep='\t')
+    for ind in ped_df['Individual'].unique():
+        jc_file = f"AFLAP_tmp/01/{f_type}Count/{ind}.jf{kmer}"
+        if os.path.exists(jc_file) and os.path.getsize(jc_file):
+            print(f"\tHash detected for {ind}. Skipping.")
+            continue
+        print(f"\tRunning jellyfish count for {ind}...")
+        ind_df = ped_df[ped_df["Individual"] == ind].dropna()
+
+        # get files to pass into jellyfish count
+        jfin = list()
+        for file in ind_df["Path"].unique():
+            if not os.path.exists(file):
+                exit(f"An error occurred: {file} for {ind} not found.")
+            jfin.append(file)
+        jfin = ' '.join(jfin)
+
+        subprocess.run(args=f"jellyfish count -m {kmer} -C -s 1G -t {threads} -o AFLAP_tmp/01/{f_type}Count/{ind}.jf{kmer} <(zcat {jfin})",
+                       shell=True, executable="/bin/bash")
+        # check if jellyfish worked
+        if not os.path.exists(jc_file) or not os.path.getsize(jc_file):
+            exit(f"An error occurred: Jellyfish for {ind} did not complete.")
+        print(f"\tHash for {ind} performed.")
+
+    print(f"Jellyfish {f_type} count complete.")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog='JELLYFISH', description="A script to obtain JELLYFISH hashes to be used when running AFLAP.")
