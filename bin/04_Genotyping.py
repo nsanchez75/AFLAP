1638bb6cfa472755fcca1cd92965402eadca8cfb
Nikko Sanchez <n.sanchez7580@gmail.com>
2023-07-12 17:46:55 -0700

    test print prog df in 04

--- bin/04_Genotyping.py
+++ bin/04_Genotyping.py
@@ -22,6 +22,7 @@ def genotype_jfq(kmer:str, LowCov:str, parent:str, lo:str, up:str, p0:str, f_typ
     ped_df = pd.read_csv(ped_file, sep='\t')
     # TODO: if this works then refactor all '.loc' stuff
     prog_df = ped_df[(ped_df["MP"] == parent) | (ped_df["FP"] == parent)]
+    print(prog_df)
     h_list = prog_df["Individual"].unique().tolist()
 
     # check if any progeny found
