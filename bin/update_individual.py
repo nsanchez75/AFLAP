327b344aaefaad876a7fe7fa374db78d683d6393
Nikko Sanchez <n.sanchez7580@gmail.com>
2023-07-15 14:19:46 -0700

    quick edit update_ind

--- bin/update_individual.py
+++ bin/update_individual.py
@@ -11,16 +11,16 @@ def remove_files(paths:list[str], parent_path:str)->None:
     for tmpf in tmpfiles:
         os.remove(tmpf)
 
-def update_individual(rem_ind, pedigree)->None:
+def update_individual(rem_ind:str, pedigree:str)->None:
     # identify the individual to be removed
     ped_df = pd.read_csv(pedigree, sep='\t', header=None, usecols=[0,1], names=["Individual", "Generation"])
     if rem_ind not in ped_df["Individual"].unique():
         exit(f"An error occurred: Unable to remove {rem_ind} since it does not exist in the pedigree file. Make sure you are using the same pedigree file from the previous run.")
 
-    ind_df = ped_df.loc[ped_df["Individual"] == rem_ind]
+    ind_df = ped_df[ped_df["Individual"] == rem_ind]
     # detect if more than one generation given to removed individual (FIXME: could have been identified in ped_analysis.py)
-    if (ind_df["Generation"].unique() != 1):
-        raise ValueError(f"{rem_ind} found to have been identified with more than one generation type.")
+    if (ind_df["Generation"].unique().size != 1):
+        exit(f"An error occurred: {rem_ind} found to have been identified with more than one generation type.")
     rem_ind_gen = int(ind_df["Generation"].unique()[0])
 
     # remove individual from all analysis files
