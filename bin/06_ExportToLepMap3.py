5d4e030640759cd85f001a35ff4ab6bfe4bf88da
Nikko Sanchez <n.sanchez7580@gmail.com>
2023-08-20 19:25:27 -0700

    refactor 06 to account for progs filtered out by LOD

--- bin/06_ExportToLepMap3.py
+++ bin/06_ExportToLepMap3.py
@@ -19,47 +19,48 @@ def create_f1_forlepmap(kmer:int)->None:
             continue
 
         # determine male and female parent
-        sex_dict = {'male': None, 'female': None}
-        if SEX == "male":     sex_dict['male'], sex_dict['female'] = G, P0
-        elif SEX == "female": sex_dict['male'], sex_dict['female'] = P0, G
+        if SEX == "male":     male, female = G, P0
+        elif SEX == "female": male, female = P0, G
 
-        data = [["CHR", "POS", f"{sex_dict['male']}x{sex_dict['female']}", f"{sex_dict['male']}x{sex_dict['female']}"],
-                ["CHR", "POS", sex_dict['male']                          , sex_dict['female']                        ],
-                ["CHR", "POS", '0'                                       , '0'                                       ],
-                ["CHR", "POS", '0'                                       , '0'                                       ],
-                ["CHR", "POS", '1'                                       , '2'                                       ],
-                ["CHR", "POS", '0'                                       , '0'                                       ]]
+        # get info from filtered genotype table
+        filtered_tsv = f"AFLAP_tmp/05/{G}_F1_m{kmer}_L{LO}_U{UP}_{P0}.Genotypes.MarkerID.Filtered.tsv"
+        if (not os.path.exists(filtered_tsv)):
+            exit(f"An error occurred: Filtered {filtered_tsv} file not found. Rerun 05_ObtainSegStats.py.")
+        ftsv_df = pd.read_csv(filtered_tsv, sep='\t')
+        ftsv_df.insert(0, "MarkerLoc", ftsv_df["MarkerID"].astype(str) + '_' + ftsv_df["MarkerLength"].astype(str))
+        ftsv_df = ftsv_df.drop(["MarkerID", "MarkerLength"], axis=1)
+
+        # add columns for male and female parents
+        if SEX == "male":
+            ftsv_df.insert(2, "Male Parent", 1)
+            ftsv_df.insert(3, "Female Parent", 0)
+        elif SEX == "female":
+            ftsv_df.insert(2, "Male Parent", 0)
+            ftsv_df.insert(3, "Female Parent", 1)
+
+        ftsv_df = ftsv_df.replace([0, 1, 2], ['1 0 0 0 0 0 0 0 0 0', '0 1 0 0 0 0 0 0 0 0', '0 0 0 0 1 0 0 0 0 0'], regex=False)
+        ftsv_df = ftsv_df.set_axis(list(lepmap_df.columns), axis=1)
+
+        # create lepmap header
+        data = [["CHR", "POS", f"{male}x{female}", f"{male}x{female}"],
+                ["CHR", "POS", male              , female            ],
+                ["CHR", "POS", '0'               , '0'               ],
+                ["CHR", "POS", '0'               , '0'               ],
+                ["CHR", "POS", '1'               , '2'               ],
+                ["CHR", "POS", '0'               , '0'               ]]
         lepmap_df = pd.DataFrame(data)
 
         # put all progeny of parent into header
         prog_df = pd.read_csv(f"AFLAP_tmp/Pedigree_F1.txt", sep='\t')
-
-        prog_df = prog_df[(prog_df["MP"].astype(str) == G) | (prog_df["FP"].astype(str) == G)]
+        prog_df = prog_df[(prog_df["MP"].astype(str).isin(male.split('_'))) | (prog_df["FP"].astype(str).isin(female.split('_')))]
+        included_progs = set(ftsv_df.columns[4:])
         for prog in prog_df["Individual"].unique():
-            added_data = pd.Series([f"{sex_dict['male']}x{sex_dict['female']}", prog,
-                        sex_dict['male'], sex_dict['female'], '0', '0'])
-            lepmap_df = pd.concat([lepmap_df, added_data], axis=1)
+            if prog in included_progs:
+                added_data = pd.Series([f"{male}x{female}", prog, male, female, '0', '0'])
+                lepmap_df = pd.concat([lepmap_df, added_data], axis=1)
 
-        # add rows from filtered tsv file to lepmap df
-        filtered_tsv = f"AFLAP_tmp/05/{G}_F1_m{kmer}_L{LO}_U{UP}_{P0}.Genotypes.MarkerID.Filtered.tsv"
-        if (not os.path.exists(filtered_tsv)):
-            exit(f"An error occurred: Filtered {filtered_tsv} file not found. Rerun 05_ObtainSegStats.py.")
-        ftsv = pd.read_csv(filtered_tsv, sep='\t')
-        ftsv.insert(0, "MarkerLoc", ftsv["MarkerID"].astype(str) + '_' + ftsv["MarkerLength"].astype(str))
-        ftsv = ftsv.drop(["MarkerID", "MarkerLength"], axis=1)
-
-        if SEX == "male":
-            ftsv.insert(2, "Male Parent", 1)
-            ftsv.insert(3, "Female Parent", 0)
-        elif SEX == "female":
-            ftsv.insert(2, "Male Parent", 0)
-            ftsv.insert(3, "Female Parent", 1)
-
-        ftsv = ftsv.replace([0, 1, 2], ['1 0 0 0 0 0 0 0 0 0', '0 1 0 0 0 0 0 0 0 0', '0 0 0 0 1 0 0 0 0 0'], regex=False)
-        ftsv = ftsv.set_axis(list(lepmap_df.columns), axis=1)
-
-        # create lepmap tsv data
-        lepmap_df = pd.concat([lepmap_df, ftsv], ignore_index=True)
+        # create lepmap data file
+        lepmap_df = pd.concat([lepmap_df, ftsv_df], ignore_index=True)
         lepmap_df.to_csv(forlepmap_file, sep='\t', header=False, index=False)
 
         if not os.path.exists(forlepmap_file):
@@ -69,7 +70,7 @@ def create_f1_forlepmap(kmer:int)->None:
 def create_f2_forlepmap(kmer:int)->None:
     male = list()
     female = list()
-    for G, LO, UP, P0, SEX in get_LA_info():
+    for G, _, _, _, SEX in get_LA_info():
         if SEX == "male": male.append(G)
         else:             female.append(G)
     male = '_'.join(male)
@@ -80,6 +81,23 @@ def create_f2_forlepmap(kmer:int)->None:
         print(f"LepMap3 info for F2 progeny has been detected. Skipping.")
         return
 
+    # get info from filtered genotype table
+    filtered_tsv = f"AFLAP_tmp/05/{male}x{female}_F2_m{kmer}.Genotypes.MarkerID.Filtered.tsv"
+    if (not os.path.exists(filtered_tsv)):
+        exit(f"An error occurred: Filtered {filtered_tsv} file not found. Rerun 05_ObtainSegStats.py.")
+    ftsv_df = pd.read_csv(filtered_tsv, sep='\t')
+    ftsv_df = ftsv_df.iloc[:, [1, 0] + list(range(2, ftsv_df.shape[1]))]
+
+    # add columns for male and female parents (F0 and F1)
+    ftsv_df.insert(2, "Male F0 Parent", 'AA')
+    ftsv_df.insert(3, "Female F0 Parent", 'BB')
+    ftsv_df.insert(4, "Male F1 Parent", 'AB')
+    ftsv_df.insert(5, "Female F1 Parent", 'AB')
+
+    # replace genotype table values to their chromosome data equivalents
+    ftsv_df = ftsv_df.replace(['XX', 'AA', 'BB', 'AB'], ['0.33 0.33 0 0 0.33 0 0 0 0 0', '1 0 0 0 0 0 0 0 0 0', '0 0 0 0 1 0 0 0 0 0', '0 1 0 0 0 0 0 0 0 0'], regex=False)
+
+    # create lepmap header
     data = [["CHR", "POS", f"{male}x{female}" , f"{male}x{female}", f"{male}x{female}", f"{male}x{female}"],
             ["CHR", "POS", male               , female            , "DUM1"            , "DUM2"            ],
             ["CHR", "POS", '0'                , '0'               , female            , female            ],
@@ -90,32 +108,15 @@ def create_f2_forlepmap(kmer:int)->None:
 
     # put all progeny into header
     prog_df = pd.read_csv(f"AFLAP_tmp/Pedigree_F2.txt", sep='\t')
-    prog_df = prog_df[(prog_df["MP"].astype(str) == male) | (prog_df["FP"].astype(str) == female)]
+    prog_df = prog_df[(prog_df["MP"].astype(str).isin(male.split('_'))) | (prog_df["FP"].astype(str).isin(female.split('_')))]
+    included_progs = set(ftsv_df.columns[6:])
     for prog in prog_df["Individual"].unique():
-        # TODO: ask Fletcher how to format the header
-        added_data = pd.Series([f"{male}x{female}", prog, "DUM1", "DUM2", '0', '0'])
-        lepmap_df = pd.concat([lepmap_df, added_data], axis=1)
+        if prog in included_progs:
+            added_data = pd.Series([f"{male}x{female}", prog, "DUM1", "DUM2", '0', '0'])
+            lepmap_df = pd.concat([lepmap_df, added_data], axis=1)
 
-    # add rows from filtered tsv file to lepmap df
-    filtered_tsv = f"AFLAP_tmp/05/{male}x{female}_F2_m{kmer}.Genotypes.MarkerID.Filtered.tsv"
-    if (not os.path.exists(filtered_tsv)):
-        exit(f"An error occurred: Filtered {filtered_tsv} file not found. Rerun 05_ObtainSegStats.py.")
-    ftsv_df = pd.read_csv(filtered_tsv, sep='\t').drop(columns=["XX Frequency", "AA Frequency", "BB Frequency", "AB Frequency"])
-    ftsv_df = ftsv_df.iloc[:, [1, 0] + list(range(2, ftsv_df.shape[1]))]
-
-    # add columns for male and female parents
-    ftsv_df.insert(2, "Male F0", 'AA')
-    ftsv_df.insert(3, "Female F0", 'BB')
-    ftsv_df.insert(4, "Male F1", 'AB')
-    ftsv_df.insert(5, "Female F1", 'AB')
-
-    ftsv_df.to_csv("test.tsv", sep='\t', index=False)
-
-    ftsv_df = ftsv_df.replace(['XX', 'AA', 'BB', 'AB'], ['0.33 0.33 0 0 0.33 0 0 0 0 0', '1 0 0 0 0 0 0 0 0 0', '0 0 0 0 1 0 0 0 0 0', '0 1 0 0 0 0 0 0 0 0'], regex=False)
-
+    # create lepmap data file
     ftsv_df = ftsv_df.set_axis(list(lepmap_df.columns), axis=1)
-
-    # create lepmap tsv data
     lepmap_df = pd.concat([lepmap_df, ftsv_df], ignore_index=True)
     lepmap_df.to_csv(forlepmap_file, sep='\t', header=False, index=False)
 
